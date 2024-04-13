ab756ac329cda46119ad2758c61bed9ad428ebab
Marc <marctaskmaker@gmail.com>
2024-04-14 03:15:21 +0800

    Updated task2 files

--- src/Resources/app/administration/src/module/fast-order/page/fast-order-list/index.js
+++ src/Resources/app/administration/src/module/fast-order/page/fast-order-list/index.js
@@ -1,67 +1,67 @@
-import template from './fast-order-list.html.twig';
+import template from "./fast-order-list.html.twig";
 
 const { Component, Mixin } = Shopware;
 const { Criteria } = Shopware.Data;
 
-Component.register('fast-order-list', {
-    template,
+Component.register("fast-order-list", {
+  template,
 
-    inject: [
-        'repositoryFactory',
-    ],
+  inject: ["repositoryFactory"],
 
-    data() {
-        return {
-            fastOrders: [],
-            fastOrderColumns: [
-                { property: 'article', label: 'Article' },
-                { property: 'quantity', label: 'Quantity' },
-                { property: 'session', label: 'Session' },
-                { property: 'created_at', label: 'Created' },
-                { property: 'updated_at', label: 'Updated' },
-            ],
-            isLoading: false,
-            showDeleteModal: false,
-        };
+  data() {
+    return {
+      fastOrders: [],
+      fastOrderColumns: [
+        { property: "product", label: "Product" },
+        { property: "quantity", label: "Quantity" },
+        { property: "session", label: "Session" },
+        { property: "created_at", label: "Created" },
+        { property: "updated_at", label: "Updated" },
+      ],
+      isLoading: false,
+      showDeleteModal: false,
+    };
+  },
+
+  computed: {
+    fastOrderRepository() {
+      return this.repositoryFactory.create("fast_order");
+    },
+  },
+
+  created() {
+    this.getFastOrders();
+  },
+
+  methods: {
+    getFastOrders: function () {
+      this.fastOrderRepository
+        .search(new Criteria(), Shopware.Context.api)
+        .then((result) => {
+          this.fastOrders = result;
+        });
     },
 
-    computed: {
-        fastOrderRepository() {
-            return this.repositoryFactory.create('fast_order');
-        },
+    onDelete(id) {
+      this.showDeleteModal = id;
     },
 
-    created() {
+    onCloseDeleteModal() {
+      this.showDeleteModal = false;
+    },
+
+    onConfirmDelete(id) {
+      this.showDeleteModal = false;
+
+      return this.fastOrderRepository.delete(id).then(() => {
         this.getFastOrders();
+      });
     },
+  },
 
-    methods: {
-        getFastOrders: function () {
-            this.fastOrderRepository.search(new Criteria(), Shopware.Context.api).then((result) => {
-                this.fastOrders = result;
-            })
-        },
-
-        onDelete(id) {
-            this.showDeleteModal = id;
-        },
-
-        onCloseDeleteModal() {
-            this.showDeleteModal = false;
-        },
-
-        onConfirmDelete(id) {
-            this.showDeleteModal = false;
-
-            return this.fastOrderRepository.delete(id).then(() => {
-                this.getFastOrders();
-            });
-        },
-    },
-
-    metaInfo() {
-        return {
-            title: this.$createTitle(),
-        };
-    },
+  metaInfo() {
+    return {
+      title: this.$createTitle(),
+    };
+  },
 });
